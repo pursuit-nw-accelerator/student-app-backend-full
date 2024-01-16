eb3bc37f892970be06921c086e95c427851eebce
Mike Boyle <mikesboyle@gmail.com>
2024-01-16 18:28:58 -0500

    return grades; remove write ops

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -2,12 +2,9 @@ const { Router } = require('express');
 const {
   getAllStudents,
   getStudentById,
-  createStudent,
-  updateStudent,
-  deleteStudent,
 } = require('../queries/studentsQueries');
 
-const { validateId, validateStudent } = require('../middleware');
+const { validateId } = require('../middleware');
 
 const studentsController = Router();
 
@@ -31,60 +28,6 @@ studentsController.get('/:id', validateId, async (request, response) => {
         .status(404)
         .json({ error: `Cannot find student with id ${id}` });
     }
-  } catch (err) {
-    response.status(500).json({ error: err.message });
-  }
-});
-
-// Part 1.1: POST /students
-studentsController.post('/', validateStudent, async (request, response) => {
-  try {
-    const { student } = request; // from middleware
-    const createdStudent = await createStudent(student);
-    response.status(201).json({ data: createdStudent });
-  } catch (err) {
-    response.status(500).json({ error: err.message });
-  }
-});
-
-// Part 1.2: PUT /students/:id
-studentsController.put(
-  '/:id',
-  validateId,
-  validateStudent,
-  async (request, response) => {
-    try {
-      const { id, student } = request; // from middleware
-
-      // First try to fetch the student; return 404 if doesn't exist
-      const persistedStudent = await getStudentById(id);
-      if (!persistedStudent) {
-        return response
-          .status(404)
-          .json({ error: `Cannot find student with id ${id}` });
-      }
-      const updatedStudent = await updateStudent(id, student);
-      response.status(200).json({ data: updatedStudent });
-    } catch (err) {
-      response.status(500).json({ error: err.message });
-    }
-  }
-);
-
-// Part 1.3: DELETE /students/:id
-studentsController.delete('/:id', validateId, async (request, response) => {
-  try {
-    const { id } = request; // from middleware
-
-    // First try to fetch the student; return 404 if doesn't exist
-    const persistedStudent = await getStudentById(Number(id));
-    if (!persistedStudent) {
-      return response
-        .status(404)
-        .json({ error: `Cannot find student with id ${id}` });
-    }
-    const deletedStudent = await deleteStudent(Number(id));
-    response.status(200).json({ data: deletedStudent });
   } catch (err) {
     response.status(500).json({ error: err.message });
   }
--- queries/studentsQueries.js
+++ queries/studentsQueries.js
@@ -9,28 +9,7 @@ const getStudentById = async (id) => {
   return students.find((student) => student.id === id);
 };
 
-const createStudent = async (student) => {
-  students.push({ id: ++lastId, ...student });
-  return students[students.length - 1];
-};
-
-const updateStudent = async (id, student) => {
-  const idx = students.findIndex((currStudent) => currStudent.id === id);
-  students[idx] = { id, ...student };
-  return students[idx];
-};
-
-const deleteStudent = async (id) => {
-  const idx = students.findIndex((currStudent) => currStudent.id === id);
-  const deletedStudent = students[idx];
-  students = students.filter((student) => student.id !== id);
-  return deletedStudent;
-};
-
 module.exports = {
   getAllStudents,
   getStudentById,
-  createStudent,
-  updateStudent,
-  deleteStudent,
 };
